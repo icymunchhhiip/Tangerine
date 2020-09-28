e48db89ba7bd282c8995252d4ae546459d017291
jc15853 <jchoi15853@ynu.ac.kr>
2020-09-28 23:51:30 +0900

    수정

--- app/src/main/java/com/sixsense/tangerine/setting/MyMarketPostActivity.java
+++ app/src/main/java/com/sixsense/tangerine/setting/MyMarketPostActivity.java
@@ -2,9 +2,11 @@ package com.sixsense.tangerine.setting;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.sixsense.tangerine.R;
 import com.sixsense.tangerine.community.item.Member;
@@ -19,6 +21,14 @@ public class MyMarketPostActivity extends AppCompatActivity implements MyMarketP
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_activity_mymarket_list);
+
+        Toolbar toolbar = findViewById(R.id.mymarket_toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
 
         Intent intent = getIntent();
         member = (Member) intent.getSerializableExtra("member");
